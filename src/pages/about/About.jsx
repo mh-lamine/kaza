6c6e73bb6c36d5f956c0ce66f8e84805b4ec4c86
mh-lamine <mlamins.ngom@gmail.com>
2024-04-11 15:37:15 +0200

    Refactor About component and add PropTypes

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,25 @@
-export default function About() {
+import bgimage from "../../assets/images/background2.png";
+import "./About.scss";
+import about from "../../assets/about.json";
+import propTypes from "prop-types";
+import Panel from "../../components/panel/Panel";
+
+const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page.</p>
+    <div className="container">
+      <img src={bgimage} />
+      <div className="about">
+        {about.map((about, index) => (
+          <Panel key={index} title={about.title} data={about.description} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
+About.propTypes = {
+  title: propTypes.string,
+  description: propTypes.string,
+};
+
+export default About;
